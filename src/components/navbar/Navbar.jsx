b52e35f9630349cb8500a9e27a4b432cb708831f
Ecenur Kaya <kaya.ecenur02@gmail.com>
2024-10-10 16:58:52 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import "./Navbar.scss"
 import Logo from "../../assets/Logo.png"
+import {IoMenu} from "react-icons/io5"
+import {IoMdClose} from "react-icons/io"
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [showNav, setShowNav] = useState(false)
   return (
     <header className='navbar'>
         <nav className='navbar__container wrapper'>
@@ -10,16 +14,31 @@ const Navbar = () => {
                 <img src={Logo} alt='logo'/>
             </a>
 
-            <ul>
-                <li><a href='#'>product</a></li>
-                <li><a href='#'>company</a></li>
-                <li><a href='#'>pricing</a></li>
-                <li><a href='#'>blog</a></li>
+            <ul className={`${showNav ? "show" : ""}`}>
+                <li>
+                    <a href='#'>product</a>
+                </li>
+
+                <li>
+                    <a href='#'>company</a>
+                </li>
+
+                <li>
+                    <a href='#'>pricing</a>
+                </li>
+
+                <li>
+                    <a href='#'>blog</a>
+                </li>
             </ul>
 
             <div className="navbar__btns">
                 <a href="#">login</a>
                 <a href="#" className="btn">register</a>
+            </div>
+
+            <div className='navbar__menu' onClick={()=>setShowNav(!showNav)}>
+                {showNav ? <IoMdClose/>:<IoMenu/>}
             </div>
         </nav>
     </header>
