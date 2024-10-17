c6e0bf2f4e7ba20c65db7c045873db08f2d91eff
Ecenur Kaya <kaya.ecenur02@gmail.com>
2024-10-17 15:09:07 +0300

    subscribe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Pricing from "./components/pricing/Pricing"
 import Status from "./components/status/Status"
+import Subscibe from "./components/subscribe/Subscibe"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <Pricing/>
     <Earnings/>
     <Status/>
+    <Subscibe/>
     </>
   )
 }
